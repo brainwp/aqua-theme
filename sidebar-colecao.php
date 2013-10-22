a23b76f3dc48fe301ecf7037361a38da9368f0d5
unknown <matiaseveraldo@gmail.com>
2013-10-22 14:48:22 -0200

    Programando menu lateral de taxonomias.

--- sidebar-colecao.php
+++ sidebar-colecao.php
@@ -6,5 +6,26 @@
  */
 ?>
 	<div id="secondary-colecao" class="widget-area" role="complementary">
-		<?php list_posts_by_taxonomy( 'itens', 'tipos' ); ?>
+
+<ul>
+		<?php $args = array(
+			'show_option_all'    => '',
+			'orderby'            => 'name',
+			'order'              => 'ASC',
+			'style'              => 'list',
+			'hide_empty'         => 1,
+			'child_of'           => 0,
+			'hierarchical'       => 1,
+			'title_li'           => '',
+			'show_option_none'   => '',
+			'number'             => null,
+			'echo'               => 1,
+			'depth'              => 0,
+			'current_category'   => 1,
+			'taxonomy'           => 'tipos',
+			'walker'             => null
+		); ?>
+		 <?php wp_list_categories( $args ); ?>
+</ul>
+
 	</div><!-- #secondary-colecao -->
