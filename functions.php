a35f69d130d6be615ac54f6a496366cc84ebdb75
unknown <matiaseveraldo@gmail.com>
2013-11-07 16:37:02 -0200

    Removendo a barra admin para usuarios que nao sao administradores.

--- functions.php
+++ functions.php
@@ -232,3 +232,11 @@ add_action('init', 'mo_options');
 function mo_options( $option ){
 	echo get_option( $option );
 }
+
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
