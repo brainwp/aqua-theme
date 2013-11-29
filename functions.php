c42caf6abe8be8227d954466987312d7c539dd39
Everaldo Matias <matiaseveraldo@gmail.com>
2013-11-29 10:47:52 -0200

    Restringindo /wp-admin apenas ao Admin.

--- functions.php
+++ functions.php
@@ -242,6 +242,18 @@ $redir = get_option('siteurl') . "/entrar";
 return $logouturl . '&amp;redirect_to=' . urlencode($redir);
 }
 
+add_action( 'admin_init', 'redirect_non_admin_users' );
+/**
+ * Redirect non-admin users to home page
+  * This function is attached to the 'admin_init' action hook.
+ */
+function redirect_non_admin_users() {
+	if ( ! current_user_can( 'manage_options' ) && '/wp-admin/admin-ajax.php' != $_SERVER['PHP_SELF'] ) {
+		wp_redirect( home_url() );
+		exit;
+	}
+}
+
 //Adiciona as Minhas Opções
 require_once (get_stylesheet_directory() . '/options/admin_options.php');
 
