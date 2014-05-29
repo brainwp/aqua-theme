d76b98405fe13273b607749cf0802f2e9d929d0a
everaldomatias <everaldo@brasa.art.br>
2014-05-29 19:14:44 -0300

    Iniciando nova proposta de single

--- single-itens.php
+++ single-itens.php
@@ -34,7 +34,7 @@
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
 							<?php global $post;
-							$terms = wp_get_post_terms( $post->ID, 'tipos');
+								$terms = wp_get_post_terms( $post->ID, 'tipos');
 							 ?> 
 						<h2 class="entry-single"><?php echo $terms[1]->name; ?></h2>
 
@@ -47,6 +47,36 @@
 					</header><!-- .entry-header -->
 
 					<div class="entry-content-single-item">
+						<?php
+                        	$args = array(
+                                'post_type' => 'attachment',
+                                'numberposts' => -1,
+                                'post_status' => null,
+                                'post_parent' => $post->ID,
+                                );
+                            
+                            $anexos = get_posts ( $args );
+                            
+                            if ( $anexos ) :
+                                foreach ( $anexos as $anexo ) : ?>
+                                
+                                <?php 
+                                    $attachment_id = $anexo->ID;
+                                    $image_attributes = wp_get_attachment_image_src( $attachment_id, 'galeria' );
+                                    $attachment_page = get_attachment_link( $attachment_id ); 
+                                    $description = $anexo->post_content;
+									$url = wp_get_attachment_url( $attachment_id ); 
+								?>
+
+								<div class="each-foto">
+									<a href=""><img src="<?php echo $image_attributes[0]; ?>" alt=""></a>
+								</div><!-- each-foto -->
+								Everaaaaa
+
+								<?php endforeach; ?>
+
+							<?php endif; ?>
+
 						<?php the_content(); ?>
 						<?php
 							wp_link_pages( array(
